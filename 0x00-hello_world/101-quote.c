20821a041fbc2de075e9e5f4e29eaa5dfb6e6c51
escaper <assaoui54@gmail.com>
2023-06-15 16:30:25 -0700

    optional quest are done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print an exact text
+ * to the std error
+ * Return: will return 1
+ *
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
