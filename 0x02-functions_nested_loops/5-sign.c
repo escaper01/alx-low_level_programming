3288384856aa89dc003f9aa6c34c8e673be73f10
escaper <assaoui54@gmail.com>
2023-06-20 15:21:29 -0700

    added 5-sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * print_sign - shuf desc
+  * Description: sin checkerr
+  * @n: sign to check
+  * Return: return 1 or 0 or -1
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+}
