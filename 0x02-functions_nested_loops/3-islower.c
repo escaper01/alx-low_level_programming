5931b2ed526a77d5d75a0443082d3d4bb2cb29ad
escaper <assaoui54@gmail.com>
2023-06-20 14:36:18 -0700

    added 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * _islower - shuf desc
+  * Description: lowercase checker
+  * Return: 1 or 0
+  */
+int _islower(int c)
+{
+	if (c >= 97)
+	{
+		return (1);
+	}
+	return (0);
+}
