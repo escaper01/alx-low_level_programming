83cfd56995da999653daf91903a6d9a572f5703a
escaper01 <assaoui54@gmail.com>
2023-06-21 00:51:39 +0100

    8 fix 3

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,47 +7,43 @@
 void jack_bauer(void)
 {
 	int hours_, hours_1, minutes_, minutes_1, hours_m;
-	
+
 	hours_m = 58;
 
 	hours_ = '0';
 
 	while (hours_ < '3')
     	{
-		if (hours_ == '2')
-		{
-			hours_m = '4';
-		}
-		hours_1 = '0';
-<<<<<<< HEAD
-			
-=======
->>>>>>> 8ba690aa52fcbf08365727a296613f1184dd6ca2
-		while (hours_1 < hours_m)
-		{
-			minutes_ = '0';
-
-			while (minutes_ < '6')
+			if (hours_ == '2')
 			{
-				minutes_1 = '0';
+				hours_m = '4';
+			}
+			hours_1 = '0';
+			while (hours_1 < hours_m)
+			{
+				minutes_ = '0';
 
-				while (minutes_1 < 58)
+				while (minutes_ < '6')
 				{
-					_putchar(hours_);
-					_putchar(hours_1);
-					_putchar(':');
-					_putchar(minutes_);
-					_putchar(minutes_1);
-					_putchar('\n');
-					minutes_1++;
+					minutes_1 = '0';
+
+					while (minutes_1 < 58)
+					{
+						_putchar(hours_);
+						_putchar(hours_1);
+						_putchar(':');
+						_putchar(minutes_);
+						_putchar(minutes_1);
+						_putchar('\n');
+						minutes_1++;
+					}
+					minutes_1 = '0';
+					minutes_++;
 				}
-				minutes_1 = '0';
-				minutes_++;
+				minutes_ = '0';
+				hours_1++;
 			}
-			minutes_ = '0';
-			hours_1++;
+			hours_1 = '0';
+			hours_++;
 		}
-		hours_1 = '0';
-		hours_++;
-			}
 }
