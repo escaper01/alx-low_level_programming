e746cb5200671e891cc03c598a731907b7ed9c52
escaper <assaoui54@gmail.com>
2023-06-20 13:57:22 -0700

    added 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - shuf desc
+  * Description: print the alphabet 10X
+  * Return: always 0
+  */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10 ; i++)
+	{
+		print_alphabet();
+	}
+}
+
