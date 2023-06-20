7c8f0f99d656931b93cc7ad2ccd2e1627c4cb272
escaper <assaoui54@gmail.com>
2023-06-20 15:44:50 -0700

    added 6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _abs- shuf desc
+  * Description: return absr
+  * @n: sign to check
+  * Return: return abs val
+  */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-1 * n);
+	}
+}
