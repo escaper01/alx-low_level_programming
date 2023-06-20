9b90aaea4990ef563154a7b552f4f17cce77cc2f
escaper01 <assaoui54@gmail.com>
2023-06-21 00:29:37 +0100

    hhh

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,34 +12,35 @@ void jack_bauer(void)
     hours_ = '0';
     while (hours_ < '3')
     {
-        if (hours_ == '2')
-        {
-            hours_m = '4';
-        }
-        hours_1 = '0';
-        while (hours_1 < hours_m)
-        {
-            minutes_ = '0';
-            while (minutes_ < '6')
-            {
-                minutes_1 = '0';
-                while (minutes_1 < 58)
-                {
-                    _putchar(hours_);
-                    _putchar(hours_1);
-                    _putchar(':');
-                    _putchar(minutes_);
-                    _putchar(minutes_1);
-                    _putchar('\n');
-                    minutes_1++;
-                }
-                minutes_1 = '0';
-                minutes_++;
-            }
-            minutes_ = '0';
-            hours_1++;
-        }
-        hours_1 = '0';
-        hours_++;
-    }
+		if (hours_ == '2')
+		{
+			hours_m = '4';
+		}
+		hours_1 = '0';
+
+		while (hours_1 < hours_m)
+		{
+			minutes_ = '0';
+			while (minutes_ < '6')
+			{
+				minutes_1 = '0';
+				while (minutes_1 < 58)
+				{
+					_putchar(hours_);
+					_putchar(hours_1);
+					_putchar(':');
+					_putchar(minutes_);
+					_putchar(minutes_1);
+					_putchar('\n');
+					minutes_1++;
+				}
+				minutes_1 = '0';
+				minutes_++;
+			}
+			minutes_ = '0';
+			hours_1++;
+		}
+		hours_1 = '0';
+		hours_++;
+			}
 }
