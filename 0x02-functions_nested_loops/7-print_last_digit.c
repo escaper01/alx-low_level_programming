ca6ec81886664623d221c9c03f92b1228a067dcc
escaper <assaoui54@gmail.com>
2023-06-20 15:55:13 -0700

    added 7-print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * print_last_digits- shuf desc
+  * Description: return last number
+  * @n: sign to check
+  * Return: return  val
+  */
+int _abs(int n)
+{
+	return (n % 10);
+}
