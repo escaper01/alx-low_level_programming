33d67dd7bd15278b689ba945c654685e0be507f1
escaper <assaoui54@gmail.com>
2023-06-20 12:09:58 -0700

    added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * main - shuf desc
+  * Description: alphabet printer 97 -> 122
+  * Return: just void
+  */
+void print_alphabet(void)
+{
+	for (int i=97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
