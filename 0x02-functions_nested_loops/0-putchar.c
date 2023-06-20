43986bfe2bcac14d7c920bb5d10460aa2e0112b4
escaper <assaoui54@gmail.com>
2023-06-20 11:51:52 -0700

    added 0-putchar final 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- * main - entry point
- * Description: prints a string with nl
- * Return: always 0 on success
- */
+  * main - suf desc
+  * Description: prints the given string
+  * Return: always 0 on success
+  */
 int main(void)
 {
-	printf("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('h');
 	return (0);
 }
