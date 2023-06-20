2918b93bea892efe3efc62ea9a2ade80baabb43c
escaper <assaoui54@gmail.com>
2023-06-20 05:52:22 -0700

    added 0._putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints a string with nl
+ * Return: always 0 on success
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
