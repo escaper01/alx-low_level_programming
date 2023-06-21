478b3d12f56a9361d9c3503ec9ddac68d9304bd1
escaper <assaoui54@gmail.com>
2023-06-21 01:29:38 -0700

    100-time-tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,12 +9,13 @@
   */
 void print_times_table(int n)
 {
-	if ( (n < 0) || (n > 15))
+	int i, j;
+	int num = 0;
+
+	if ((n < 0) || (n > 15))
 	{
 		return;
 	}
-	int i, j;
-	int num = 0;
 
 	for (i = 0; i <= n; i++)
 	{
