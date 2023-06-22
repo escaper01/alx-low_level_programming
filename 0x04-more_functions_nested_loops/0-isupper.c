4aea27abf394f96e090090ee5345a00db2efe756
escaper01 <assaoui54@gmail.com>
2023-06-22 15:25:49 +0100

    added 0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - shuf desc
+ * @c: character to be checked
+ * Description: checker for uppercase
+ * Return: 1 or 0
+*/
+
+int _isupper(int c)
+{
+	return (c <= 90);
+}
