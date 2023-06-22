0e66e981f382954aac440f7ced063a11f2ed9dbe
escaper <assaoui54@gmail.com>
2023-06-22 16:51:18 -0700

    mai

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - printii numbers
+ * Description: shuf lfuq
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	while (num != 9)
+	{
+		_putchar(num);
+		num++;
+	}
+	_putchar('\n');
+}
