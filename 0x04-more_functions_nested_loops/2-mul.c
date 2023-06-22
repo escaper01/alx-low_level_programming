0dd19e281c83c1aaaa858e0134781ae391c1cb86
escaper <assaoui54@gmail.com>
2023-06-22 16:42:08 -0700

    aw

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - int mul(int a, int b);
+ * Description: multiply a * b
+ * @a: num 1
+ * @b: num 2
+ * Return: the result of te op
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
