71c5cba0d632a045e6ebc0246cc04271b1cf6359
escaper <assaoui54@gmail.com>
2023-06-22 18:18:54 -0700

    emmmmmmm

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,6 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-void print_square(int size)
+void print_square(int size);
 void print_line(int n);
 void print_diagonal(int n);
