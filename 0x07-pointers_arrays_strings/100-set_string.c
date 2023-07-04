12bedf25ab55f4d08be4832e783f034f2f6b311b
escaper <assaoui54@gmail.com>
2023-07-04 09:13:18 +0100

    op exercise

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - set the value of a ptr to a char
+ * @s: source ptr
+ * @to: target ptr
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
