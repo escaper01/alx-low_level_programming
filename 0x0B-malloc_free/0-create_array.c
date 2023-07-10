e5e691aed465dfc44ce82e974a8b0ba64b6c12fa
escaper <assaoui54@gmail.com>
2023-07-10 14:11:55 -0700

    woow 9

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,15 +11,17 @@
 
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+	char *arr = malloc(sizeof(char) * size);
+
+
 	if (!size)
 		return (NULL);
 
-	char *arr = malloc(sizeof(char) * size);
 
 	if (!arr)
 		return (NULL);
 
-	unsigned int i;
 
 	for (i = 0; i < size; i++)
 	{
