0a3b02d4fb8bfdfa9f3c61a62f5f83a7f83a6154
escaper <assaoui54@gmail.com>
2023-07-10 14:35:13 -0700

    woow 100

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,11 +12,13 @@ char *_strdup(char *str)
 {
 	int cur = 0;
 	int c = 0;
+	char *str_copy;
+
 	while (str[c])
 	{
 		c++;
 	}
-	char *str_copy = malloc(sizeof(char) * (c + 1));
+	str_copy = malloc(sizeof(char) * (c + 1));
 
 	if (!str)
 		return (NULL);
