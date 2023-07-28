1bfe722c4ef7720ac600a636e287c2136b704c8c
escaper <assaoui54@gmail.com>
2023-07-28 01:15:02 +0100

    ssssssss

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,11 +1,5 @@
-#ifndef LINKEDlISTS_H
-#define LINKEDlISTS_H
-
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+#ifndef CUSTM_H
+#define CUSTM_H
 
 /**
  * struct list_s - singly linked list
@@ -21,4 +15,10 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
 #endif
