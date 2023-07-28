aef1170fee2442e56e3539aaf3cc179f5a57e983
escaper <assaoui54@gmail.com>
2023-07-28 01:44:59 +0100

    aaaa

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include "lists.h"
-
+#include <string.h>
 
 /**
  * add_node - add node to the start
@@ -12,7 +12,6 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-
 	unsigned int i;
 	list_t *new_node;
 
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * add_node_end - print num of elements
