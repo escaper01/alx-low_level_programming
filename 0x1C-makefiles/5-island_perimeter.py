83ca1262e36e4bddaa5090af3f7fdea31a498b33
escaper <assaoui54@gmail.com>
2023-11-22 21:39:09 +0100

    emmmm2

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,7 +3,7 @@
 
 
 def island_perimeter(grid):
-    """ calculate he perimeter of an island """
+    """ calculate the perimeter of an island """
     perimeter = 0
     for x, i in enumerate(grid):
         for y, j in enumerate(i):
@@ -11,18 +11,16 @@ def island_perimeter(grid):
                 # print(x,y, left_side, top_side,
                 # right_side, bottom_side)
                 # checkin the top side
-                top_side = [x-1 if x-1 >= 0 else None, y]
+                top_side = [x-1, y]
                 # checkin the bottom side
-                bottom_side = [x+1 if x+1 < len(grid) else None, y]
+                bottom_side = [x+1, y]
                 # checkin the left side
-                left_side = [x, y-1 if y-1 >= 0 else None]
+                left_side = [x, y-1]
                 # checkin the right side
-                right_side = [x, y+1 if y+1 < len(grid[x]) else None]
+                right_side = [x, y+1]
                 all_side = [left_side, top_side,
                             right_side, bottom_side]
                 for side in all_side:
-                    if None not in side:
-                        if grid[side[0]][side[1]] == 0:
-                            perimeter += 1
+                    if grid[side[0]][side[1]] == 0:
+                        perimeter += 1
     return perimeter
-
