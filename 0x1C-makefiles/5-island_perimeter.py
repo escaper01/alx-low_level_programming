02610a0fe88aab9cba03df4505a866605c61ae1d
escaper <assaoui54@gmail.com>
2023-11-22 21:31:08 +0100

    fix out of bond

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,22 +1,36 @@
 #!/usr/bin/python3
-""" just the doc for this module """
+""" some docs for modules """
+
+
 def island_perimeter(grid):
-        """ calculate he perimeter of an island """
-        perimeter = 0
-        for x,i in enumerate(grid):
-                for y,j in enumerate(i):
-                        if (j == 1):
-                                # print(x,y, left_side, top_side, right_side, bottom_side)
-                                # checkin the top side
-                                top_side = [x-1, y]
-                                # checkin the bottom side
-                                bottom_side = [x+1, y]
-                                # checkin the left side
-                                left_side = [x, y-1]
-                                # checkin the right side
-                                right_side = [x, y+1]
-                                all_side = [left_side, top_side, right_side, bottom_side]
-                                for side in all_side:
-                                        if grid[side[0]][side[1]] == 0:
-                                                perimeter += 1
-        return perimeter
+    """ calculate he perimeter of an island """
+    perimeter = 0
+    for x, i in enumerate(grid):
+        for y, j in enumerate(i):
+            if (j == 1):
+                # print(x,y, left_side, top_side,
+                # right_side, bottom_side)
+                # checkin the top side
+                top_side = [x-1 if x-1>=0 else None, y]
+                # checkin the bottom side
+                bottom_side = [x+1 if x+1<len(grid) else None, y]
+                # checkin the left side
+                left_side = [x, y-1 if y-1>=0 else None]
+                # checkin the right side
+                right_side = [x, y+1 if y+1<len(grid[x]) else None]
+                all_side = [left_side, top_side,
+                            right_side, bottom_side]
+                for side in all_side:
+                    if None not in side:
+                        if grid[side[0]][side[1]] == 0:
+                            perimeter += 1
+    return perimeter
+
+grid = [
+[0, 0, 0, 0, 0, 0],
+[0, 1, 0, 0, 0, 0],
+[0, 1, 0, 0, 0, 0],
+[0, 1, 1, 1, 0, 0],
+[0, 0, 0, 0, 0, 0]
+]
+print(island_perimeter(grid))
