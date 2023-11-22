b8adb7460d3ccc01cc0d98af5345eb2b0dad8f7e
escaper <assaoui54@gmail.com>
2023-11-22 21:42:14 +0100

    another try

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,16 +11,19 @@ def island_perimeter(grid):
                 # print(x,y, left_side, top_side,
                 # right_side, bottom_side)
                 # checkin the top side
-                top_side = [x-1, y]
+                top_side = [x-1 if x-1>=0 else None, y]
                 # checkin the bottom side
-                bottom_side = [x+1, y]
+                bottom_side = [x+1 if x+1<len(grid) else None, y]
                 # checkin the left side
-                left_side = [x, y-1]
+                left_side = [x, y-1 if y-1>=0 else None]
                 # checkin the right side
-                right_side = [x, y+1]
+                right_side = [x, y+1 if y+1<len(grid[x]) else None]
                 all_side = [left_side, top_side,
                             right_side, bottom_side]
                 for side in all_side:
-                    if grid[side[0]][side[1]] == 0:
+                    if None in side:
                         perimeter += 1
+                    else:
+                        if grid[side[0]][side[1]] == 0:
+                            perimeter += 1
     return perimeter
